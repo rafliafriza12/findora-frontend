35440720ff0f4c4bf04ccc98335886b492060d2a
rafliafriza12 <rafliafriza90@gmail.com>
2024-11-26 20:42:26 +0700

    feat: change UI

--- frontpage/app/components/ListCard.tsx
+++ frontpage/app/components/ListCard.tsx
@@ -3,12 +3,18 @@ import NoPhoto from "../../public/assets/no-photos.png";
 import Link from "next/link";
 import Image from "next/image";
 const ListCard: React.FC<ListCardProps> = ({ item }) => {
+  const imageSrc: string =
+    item?.imageSrc !== "Gambar tidak ditemukan"
+      ? item?.imageSrc.split("?")[0]
+      : "Gambar tidak ditemukan";
+  const content: string = item?.paragraph.slice(0, 50);
+
   return (
     <div className=" w-full h-[30vh] box-border flex justify-between gap-5  font-sora text-white rounded-[16px] border-[1px] border-[#3c3c77] bg-[#15152e] duration-300 group">
       <div className=" w-[40%] h-full relative overflow-hidden rounded-l-[16px]">
-        {item?.imageSrc !== "Gambar tidak ditemukan" ? (
+        {imageSrc !== "Gambar tidak ditemukan" ? (
           <Image
-            src={item?.imageSrc}
+            src={imageSrc}
             alt="Image"
             layout="fill"
             objectFit="cover"
@@ -26,9 +32,7 @@ const ListCard: React.FC<ListCardProps> = ({ item }) => {
       </div>
       <div className="flex flex-col gap-3 w-[59%] px-6 py-9">
         <h1 className=" font-semibold text-lg">{item?.title}</h1>
-        <p className=" text-justify text-gray-300">
-          {item?.paragraph?.slice(0, 50)}[....]
-        </p>
+        <p className=" text-justify text-gray-300">{content}[....]</p>
         <Link
           className=" hover:underline text-[#ada7cd]"
           href={`/pages/result/berita/${item?.document_id}`}
